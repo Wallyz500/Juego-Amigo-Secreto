4eb139703ecbae72df5bf00f8ba5de297ee14bee
Wallyz <WallyzPerez@outlook.es>
2025-02-24 22:36:08 -0400

    actualizando el codigo js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos =[]
+let amigos = [];
 
+console.log(amigos);
+
+function agregarAmigo() {
+    let amigosAgregados = document.getElementById('amigo').value;
+    amigos.push(amigosAgregados);
+    limpiarTexto();
+}
+
+function limpiarTexto() {
+    document.getElementById('amigo').value = '';
+}
